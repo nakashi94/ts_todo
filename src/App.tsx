6883d04eefbb58c433c3800301aa7cd888e5d646
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-02 14:08:24 +0900

    logout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { db } from "./firebase";
 import AddToPhotosIcon from "@material-ui/icons/AddToPhotos"
 import TaskItem from "./TaskItem"
 import { makeStyles } from "@material-ui/styles";
+import { auth } from "./firebase"
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 const useStyles = makeStyles({
   field: {
@@ -17,10 +19,18 @@ const useStyles = makeStyles({
   },
 });
 
-const App: React.FC = () => {
+const App: React.FC = (props: any) => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
   const [input, setInput] = useState("");
   const classes = useStyles();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((user) => {
+      !user && props.history.push("login");
+    });
+    return () => unSub();
+  });
+
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
@@ -38,6 +48,18 @@ const newTask = (e: React.MouseEvent<HTMLButtonElement>)=>{
   return (
     <div className={styles.app__root}>
       <h1>Todo App by React/Firebase</h1>
+      <button className={styles.app__logout}
+      onClick={
+        async () => {
+          try {await auth.signOut();
+          props.history.push("login");
+        } catch (error: any) {
+          alert(error.message);
+        }
+      }}
+      >
+        <ExitToAppIcon />
+      </button>
       <br />
       <FormControl>
         <TextField
